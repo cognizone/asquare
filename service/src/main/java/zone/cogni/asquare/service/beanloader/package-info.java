ef16a1ce20f4f5daf66099a700b6865cf08557e1
natan-cox-cognizone <81639894+natan-cox-cognizone@users.noreply.github.com>
2021-10-05 20:37:16 +0200

    delete everything related to beanloader

--- service/src/main/java/zone/cogni/asquare/service/beanloader/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Basic idea is to be able to configure main services used in asquare with RDF.
- *
- * Idea is on hold for now!
- */
-@Deprecated
-package zone.cogni.asquare.service.beanloader;
