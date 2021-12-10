ac00fff740c7c7193e6682ba37a16bd087d88370
natan-cox-cognizone <81639894+natan-cox-cognizone@users.noreply.github.com>
2021-12-10 21:12:59 +0100

    fix by keeping it one query but multiple insert data queries separated by ;

--- access/src/main/java/zone/cogni/asquare/edit/delta/Delta.java
+++ access/src/main/java/zone/cogni/asquare/edit/delta/Delta.java
@@ -1,28 +1,31 @@
 package zone.cogni.asquare.edit.delta;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Statement;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class Delta {
 
+  // only changeable for tests; should normally not change
+  public static int PARTITION_SIZE = 1000;
+
   public static String convertStatementsToSparql(Collection<Statement> statements) {
     SparqlVisitor sparqlVisitor = SparqlVisitor.instance();
     return statements.stream()
-            .map(statement -> "\n\t\t" +
-                              statement.getSubject().visitWith(sparqlVisitor) +
-                              " " +
-                              statement.getPredicate().visitWith(sparqlVisitor) +
-                              " " +
-                              statement.getObject().visitWith(sparqlVisitor))
-            .collect(Collectors.joining("."));
+                     .map(statement -> "\n\t\t" +
+                                       statement.getSubject().visitWith(sparqlVisitor) +
+                                       " " +
+                                       statement.getPredicate().visitWith(sparqlVisitor) +
+                                       " " +
+                                       statement.getObject().visitWith(sparqlVisitor))
+                     .collect(Collectors.joining("."));
   }
 
   public abstract List<Statement> getAddStatements();
@@ -39,28 +42,30 @@ public abstract class Delta {
   }
 
   public String getSparql() {
-    return Stream.of(getDeleteSparql(), getInsertSparql())
-            .filter(Objects::nonNull)
-            .collect(Collectors.joining(";\n"));
+    return Stream.concat(getDeleteSparql(), getInsertSparql())
+                 .collect(Collectors.joining(";\n"));
   }
 
-  private String getDeleteSparql() {
+  private Stream<String> getDeleteSparql() {
     List<Statement> removeStatements = getRemoveStatements();
-    if (removeStatements.isEmpty()) return null;
+    if (removeStatements.isEmpty()) return Stream.empty();
 
-    return "DELETE DATA { \n" +
-           convertStatementsToSparql(removeStatements) + "\n" +
-           "} \n";
+    return Lists.partition(removeStatements, PARTITION_SIZE)
+                .stream()
+                .map(statements -> "DELETE DATA { \n" +
+                                   convertStatementsToSparql(statements) + "\n" +
+                                   "} \n");
   }
 
-  private String getInsertSparql() {
+  private Stream<String> getInsertSparql() {
     List<Statement> addStatements = getAddStatements();
-    if (addStatements.isEmpty()) return null;
+    if (addStatements.isEmpty()) return Stream.empty();
 
-    return "INSERT DATA { \n" +
-           convertStatementsToSparql(addStatements) + "\n" +
-           "} \n";
+    return Lists.partition(addStatements, PARTITION_SIZE)
+                .stream()
+                .map(statements -> "INSERT DATA { \n" +
+                                   convertStatementsToSparql(statements) + "\n" +
+                                   "} \n");
   }
-
 
 }
