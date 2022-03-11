a2b517c675f50925814a1dd119acc96630616782
straton <straton.samaridis@cogni.zone>
2022-03-11 11:23:42 +0100

    small revert for java.util imports expansion

--- service/src/main/java/zone/cogni/asquare/service/index/IndexService.java
+++ service/src/main/java/zone/cogni/asquare/service/index/IndexService.java
@@ -24,7 +24,12 @@ import zone.cogni.sem.jena.model.ResultSetDto;
 
 import java.io.IOException;
 import java.time.ZonedDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
