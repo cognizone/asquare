1cf9b6f89d48887a26fbeb9490155f62d6670b04
Natan Cox <81639894+natan-cox-cognizone@users.noreply.github.com>
2022-04-20 13:53:36 +0200

    upgrade of libraries; replace junit tests; fix error handling of index deletion; delete SimpleElasticRepository.java (#78)

--- access/src/test/java/zone/cogni/asquare/edit/delta/DeltaTest.java
+++ access/src/test/java/zone/cogni/asquare/edit/delta/DeltaTest.java
@@ -1,29 +1,28 @@
 package zone.cogni.asquare.edit.delta;
 
-import org.apache.jena.query.QueryParseException;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
 import zone.cogni.asquare.triplestore.jenamemory.InternalRdfStoreService;
-import zone.cogni.libs.jena.utils.JenaUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class DeltaTest {
 
   private static final Logger log = LoggerFactory.getLogger(DeltaTest.class);
 
+  /**
+   * Even 200.000 triple insert now works!?
+   */
   @Test
   public void huge_sparql_update_in_one_insert() {
     // given
     InternalRdfStoreService rdfStore = new InternalRdfStoreService();
-    Model addModel = getModel();
+    Model addModel = getModel(200_000);
 
     Delta.PARTITION_SIZE = 1_000_000;
     Delta delta = new DeltaFake(addModel.listStatements().toList(), null);
@@ -31,19 +30,15 @@ public class DeltaTest {
     log.info("(huge_sparql_update_in_one_insert) sparql size: {}", sparql.length());
 
     // when
-    assertThatThrownBy(() -> rdfStore.executeUpdateQuery(sparql))
-            // then
-            .isInstanceOf(RuntimeException.class)
-            .hasMessageContaining("Update SPARQL failed")
-            .getCause().isInstanceOf(QueryParseException.class)
-    ;
+    rdfStore.executeUpdateQuery(sparql);
+    assertThat(rdfStore.getModel().size()).isEqualTo(200_000);
   }
 
   @Test
   public void huge_sparql_update_in_many_inserts() {
     // given
     InternalRdfStoreService rdfStore = new InternalRdfStoreService();
-    Model addModel = getModel();
+    Model addModel = getModel(20_000);
 
     Delta.PARTITION_SIZE = 1000;
     Delta delta = new DeltaFake(addModel.listStatements().toList(), null);
@@ -57,9 +52,9 @@ public class DeltaTest {
     assertThat(rdfStore.getModel().size()).isEqualTo(20000);
   }
 
-  private Model getModel() {
+  private Model getModel(int size) {
     Model result = ModelFactory.createDefaultModel();
-    for (int i =1; i <= 20000; i++) {
+    for (int i = 1; i <= size; i++) {
       result.add(ResourceFactory.createStatement(
               ResourceFactory.createResource("http://demo.com/uri/" + i),
               ResourceFactory.createProperty("http://demo.com/property"),
