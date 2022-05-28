cac5689bbb30770e4b19afd77878e7dfb3a10c86
natan-cox-cognizone <81639894+natan-cox-cognizone@users.noreply.github.com>
2022-05-28 13:16:06 +0200

    shacl to json conversion: first draft including tests

---
+++ cube/src/main/java/zone/cogni/asquare/cube/shacl/ShaclToConversionProfile.java
@@ -0,0 +1,189 @@
+package zone.cogni.asquare.cube.shacl;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import zone.cogni.asquare.access.shacl.Shacl;
+import zone.cogni.asquare.cube.convertor.json.CompactConversionProfile;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ShaclToConversionProfile implements Function<Model, CompactConversionProfile> {
+
+  @Override
+  @Nonnull
+  public CompactConversionProfile apply(@Nonnull Model shacl) {
+    // TODO think a namespace normalizer?
+    CompactConversionProfile profile = new CompactConversionProfile();
+    profile.setPrefixes(shacl.getNsPrefixMap());
+    profile.setTypes(calculateTypes(profile, shacl));
+    return profile;
+  }
+
+  @Nonnull
+  private List<CompactConversionProfile.Type> calculateTypes(@Nonnull CompactConversionProfile profile,
+                                                             @Nonnull Model shacl) {
+    // TODO add a check for ShapesGraph
+    return shacl.listStatements(null, RDF.type, Shacl.NodeShape)
+                .toList()
+                .stream()
+                .map(Statement::getSubject)
+                .map(nodeShape -> calculateType(profile, shacl, nodeShape))
+                .collect(Collectors.toList());
+  }
+
+  @Nonnull
+  private CompactConversionProfile.Type calculateType(@Nonnull CompactConversionProfile profile,
+                                                      @Nonnull Model shacl,
+                                                      @Nonnull Resource nodeShape) {
+    CompactConversionProfile.Type type = new CompactConversionProfile.Type();
+    type.setSuperClasses(calculateSuperClasses(profile, shacl, nodeShape));
+    type.setId(calculateTargetClassId(profile, nodeShape));
+    type.setType(calculateRdfType(nodeShape));
+    type.setAttributes(calculateAttributes(profile, shacl, nodeShape));
+    return type;
+  }
+
+  @Nonnull
+  private String calculateTargetClassId(@Nonnull CompactConversionProfile profile,
+                                        @Nonnull Resource shape) {
+    Resource resource = shape.getPropertyResourceValue(Shacl.targetClass);
+    return convertResourceToId(profile, resource);
+  }
+
+  @Nonnull
+  private String convertResourceToId(@Nonnull CompactConversionProfile profile,
+                                     @Nonnull Resource resource) {
+    String uri = resource.getURI();
+    Optional<String> optionalId = profile.getPrefixes()
+                                         .entrySet()
+                                         .stream()
+                                         .filter(entry -> uri.startsWith(entry.getValue()))
+                                         .map(entry -> shortenUri(uri, entry))
+                                         .findFirst();
+
+    return optionalId.orElse(uri);
+  }
+
+  private String shortenUri(String uri, Map.Entry<String, String> prefix) {
+    String localPart = StringUtils.substringAfter(uri, prefix.getValue());
+    return prefix.getKey() + ":" + localPart;
+  }
+
+  @Nonnull
+  private String calculateRdfType(@Nonnull Resource nodeShape) {
+    return nodeShape.getPropertyResourceValue(Shacl.targetClass).getURI();
+  }
+
+  @Nonnull
+  private Set<String> calculateSuperClasses(@Nonnull CompactConversionProfile profile,
+                                            @Nonnull Model shacl,
+                                            @Nonnull Resource nodeShape) {
+    return getSubclasses(shacl, nodeShape)
+            .stream()
+            .map(superClass -> calculateTargetClassId(profile, superClass))
+            .collect(Collectors.toSet());
+  }
+
+  @Nonnull
+  private List<Resource> getSubclasses(@Nonnull Model shacl,
+                                       @Nonnull Resource nodeShape) {
+    return shacl.listStatements(nodeShape, RDFS.subClassOf, (RDFNode) null)
+                .toList()
+                .stream()
+                .map(Statement::getObject)
+                .map(RDFNode::asResource)
+                .collect(Collectors.toList());
+  }
+
+  @Nonnull
+  private List<CompactConversionProfile.Attribute> calculateAttributes(@Nonnull CompactConversionProfile profile,
+                                                                       @Nonnull Model shacl,
+                                                                       @Nonnull Resource nodeShape) {
+    return nodeShape.listProperties(Shacl.property)
+                    .toList()
+                    .stream()
+                    .map(statement -> statement.getObject().asResource())
+                    .map(propertyShape -> calculateAttribute(profile, shacl, nodeShape, propertyShape))
+                    .collect(Collectors.toList());
+  }
+
+  @Nonnull
+  private CompactConversionProfile.Attribute calculateAttribute(@Nonnull CompactConversionProfile profile,
+                                                                @Nonnull Model shacl,
+                                                                @Nonnull Resource nodeShape,
+                                                                @Nonnull Resource propertyShape) {
+
+    CompactConversionProfile.Attribute attribute = new CompactConversionProfile.Attribute();
+    attribute.setId(calculatePathId(profile, propertyShape));
+    attribute.setProperty(calculateProperty(propertyShape));
+    attribute.setSingle(calculateSingle(propertyShape));
+    attribute.setType(calculateAttributeType(propertyShape));
+    attribute.setInverse(false);
+    return attribute;
+  }
+
+  @Nonnull
+  private String calculatePathId(@Nonnull CompactConversionProfile profile,
+                                 @Nonnull Resource propertyShape) {
+    Resource resource = propertyShape.getPropertyResourceValue(Shacl.path);
+    return convertResourceToId(profile, resource);
+  }
+
+  @Nonnull
+  private String calculateProperty(@Nonnull Resource propertyShape) {
+    return propertyShape.getPropertyResourceValue(Shacl.path).getURI();
+  }
+
+  @SuppressWarnings("UnnecessaryLocalVariable")
+  private boolean calculateSingle(Resource propertyShape) {
+    if (!propertyShape.hasProperty(Shacl.maxCount))
+      return false;
+
+    List<Long> maxCounts = propertyShape.listProperties(Shacl.maxCount)
+                                        .toList()
+                                        .stream()
+                                        .map(Statement::getObject)
+                                        .map(RDFNode::asLiteral)
+                                        .map(Literal::getLong)
+                                        .collect(Collectors.toList());
+
+    if (maxCounts.size() != 1)
+      throw new RuntimeException("expected exactly one maxCount: " + maxCounts);
+
+    boolean isSingle = maxCounts.get(0) == 1;
+    return isSingle;
+  }
+
+  private CompactConversionProfile.Attribute.Type calculateAttributeType(Resource propertyShape) {
+    if (propertyShape.hasProperty(Shacl.nodeKind)) {
+      Resource nodeKind = propertyShape.getPropertyResourceValue(Shacl.nodeKind);
+
+      if (nodeKind.equals(Shacl.NodeKind.Literal))
+        return CompactConversionProfile.Attribute.Type.datatype;
+      else if (nodeKind.equals(Shacl.NodeKind.IRI))
+        return CompactConversionProfile.Attribute.Type.object;
+    }
+
+    if (propertyShape.hasProperty(Shacl.datatype)) {
+      return CompactConversionProfile.Attribute.Type.datatype;
+    }
+    else if (propertyShape.hasProperty(Shacl.classP)) {
+      return CompactConversionProfile.Attribute.Type.object;
+    }
+
+    return CompactConversionProfile.Attribute.Type.mix;
+  }
+
+}
---
+++ cube/src/test/java/zone/cogni/asquare/cube/shacl/ShaclToConversionProfileTest.java
@@ -0,0 +1,97 @@
+package zone.cogni.asquare.cube.shacl;
+
+import org.apache.jena.rdf.model.Model;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import zone.cogni.asquare.cube.convertor.json.CompactConversionProfile;
+import zone.cogni.libs.jena.utils.JenaUtils;
+
+import javax.annotation.Nonnull;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ShaclToConversionProfileTest {
+
+  private final ShaclToConversionProfile shaclToConversionProfile = new ShaclToConversionProfile();
+
+  @Test
+  public void simple_person_shacl_conversion() {
+    // given
+    Model personShacl = getShaclModel("simple-person.shacl.ttl");
+
+    // when
+    CompactConversionProfile profile = shaclToConversionProfile.apply(personShacl);
+
+    // then
+    assertThat(profile.getPrefixes()).hasSize(6);
+    assertThat(profile.getTypes()).hasSize(1);
+
+    CompactConversionProfile.Type person = profile.getById("ex:Person");
+    assertThat(person).isNotNull();
+    assertThat(person.getId()).isEqualTo("ex:Person");
+    assertThat(person.getType()).isEqualTo("http://example.org/person#Person");
+    assertThat(person.getSuperClasses()).isEmpty();
+    assertThat(person.getAttributes()).hasSize(5);
+
+    CompactConversionProfile.Attribute firstName = person.getById("ex:firstName");
+    assertThat(firstName).isNotNull();
+    assertThat(firstName.getProperty()).isEqualTo("http://example.org/person#firstName");
+    assertThat(firstName.getType()).isEqualTo(CompactConversionProfile.Attribute.Type.mix);
+    assertThat(firstName.isSingle()).isTrue();
+  }
+
+  @Test
+  public void attribute_type_shacl_conversion() {
+    // given
+    Model personShacl = getShaclModel("attribute-type-person.shacl.ttl");
+
+    // when
+    CompactConversionProfile profile = shaclToConversionProfile.apply(personShacl);
+
+    // then
+    CompactConversionProfile.Type personType = profile.getById("ex:Person");
+    assertThat(personType.getAttributes()).hasSize(6);
+
+    CompactConversionProfile.Attribute spouse = personType.getById("ex:spouse");
+    assertThat(spouse).isNotNull();
+    assertThat(spouse.getType()).isEqualTo(CompactConversionProfile.Attribute.Type.object);
+
+    CompactConversionProfile.Attribute firstName = personType.getById("ex:firstName");
+    assertThat(firstName).isNotNull();
+    assertThat(firstName.getType()).isEqualTo(CompactConversionProfile.Attribute.Type.datatype);
+
+    CompactConversionProfile.Attribute lastName = personType.getById("ex:lastName");
+    assertThat(lastName).isNotNull();
+    assertThat(firstName.getType()).isEqualTo(CompactConversionProfile.Attribute.Type.datatype);
+  }
+
+  @Test
+  public void attribute_cardinality_shacl_conversion() {
+    // given
+    Model personShacl = getShaclModel("attribute-cardinality-person.shacl.ttl");
+
+    // when
+    CompactConversionProfile profile = shaclToConversionProfile.apply(personShacl);
+
+    // then
+    CompactConversionProfile.Type personType = profile.getById("ex:Person");
+    assertThat(personType.getAttributes()).hasSize(8);
+
+    CompactConversionProfile.Attribute sons = personType.getById("ex:sons");
+    assertThat(sons).isNotNull();
+    assertThat(sons.getType()).isEqualTo(CompactConversionProfile.Attribute.Type.object);
+    assertThat(sons.isSingle()).isFalse();
+
+    CompactConversionProfile.Attribute daughters = personType.getById("ex:daughters");
+    assertThat(daughters).isNotNull();
+    assertThat(daughters.getType()).isEqualTo(CompactConversionProfile.Attribute.Type.object);
+    assertThat(daughters.isSingle()).isFalse();
+  }
+
+  @Nonnull
+  private Model getShaclModel(String file) {
+    Resource resource = new ClassPathResource("shacl/" + file);
+    return JenaUtils.read(resource);
+  }
+}
