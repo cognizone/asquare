b475f45882ea4c482e1d14074723b77806229330
natan-cox-cognizone <81639894+natan-cox-cognizone@users.noreply.github.com>
2023-01-26 12:36:09 +0100

    fix method to make it work with JDK 8; removed class

--- access/src/main/java/zone/cogni/asquare/Utils.java
+++
@@ -1,9 +0,0 @@
-package zone.cogni.asquare;
-
-@Deprecated
-public class Utils {
-  @Deprecated
-  public static String toJson(Object object) {
-    throw new UnsupportedOperationException("Please use JsonHelper.toJson(object). It should be faster too.");
-  }
-}
