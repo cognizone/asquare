3ed9a2b12c898bdf3b1ffb1c7a21c67042ee8ca7
natan-cox-cognizone <natan.cox@cogni.zone>
2023-09-05 15:09:31 +0200

    fix some javadoc issues

--- access/src/main/java/zone/cogni/asquare/elastic/access/ElasticAccessService.java
+++ access/src/main/java/zone/cogni/asquare/elastic/access/ElasticAccessService.java
@@ -7,9 +7,9 @@ import zone.cogni.asquare.rdf.TypedResource;
 import java.util.Optional;
 
 /**
- *  todo add and implement method to retrieve all resources for a given set of uri's on access layer! performance -> e.g. only one query in elastic
- *  todo support find by attribute -> find all possible values for a given attribute (e.g to support select boxes)
- *  todo attributes representing relations -> inverse indexing to load and retrieve related documents (resources) directly (~ tagging properties)
+ *  todo add and implement method to retrieve all resources for a given set of uri's on access layer! performance {@code ->} e.g. only one query in elastic
+ *  todo support find by attribute {@code ->} find all possible values for a given attribute (e.g to support select boxes)
+ *  todo attributes representing relations {@code ->} inverse indexing to load and retrieve related documents (resources) directly (~ tagging properties)
  *  todo investigate script fields in elastic
  *  todo investigate inner hits (parent/child, nested) and mappings
  */
