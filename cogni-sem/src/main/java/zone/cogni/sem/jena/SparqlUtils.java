a36799146028c277da8b7ef3237eebd504f15340
christina_spachiou <christina.spachiou@cogni.zone>
2024-01-30 16:28:23 +0100

    escape from list https://www.w3.org/TR/sparql11-query/#grammarEscapes plus test cases

--- cogni-sem/src/main/java/zone/cogni/sem/jena/SparqlUtils.java
+++ cogni-sem/src/main/java/zone/cogni/sem/jena/SparqlUtils.java
@@ -1,13 +1,16 @@
 package zone.cogni.sem.jena;
 
-import org.apache.commons.lang3.StringUtils;
-
 public enum SparqlUtils {
   ;
 
-  public static String escapeLiteral(String literal) {
-    return StringUtils.replace(literal, "'", "\\'")
-                      .replace("\n", "\\n");
-
+  public static String escapeString(String literal) {
+    return literal.replace("\\", "\\\\") // Escape backslash, needs to be first otherwise it will escape the backslashes of the already escaped literals
+                  .replace("\t", "\\t")  // Escape tab
+                  .replace("\b", "\\b")  // Escape backspace
+                  .replace("\n", "\\n")  // Escape newline
+                  .replace("\r", "\\r")  // Escape carriage return
+                  .replace("\f", "\\f")  // Escape formfeed
+                  .replace("'", "\\'")   // Escape single quote
+                  .replace("\"", "\\\""); // Escape double quote
   }
 }
--- cogni-sem/src/test/java/zone/cogni/sem/jena/SparqlUtilsTest.java
+++ cogni-sem/src/test/java/zone/cogni/sem/jena/SparqlUtilsTest.java
@@ -5,39 +5,54 @@ import org.junit.jupiter.api.Test;
 import zone.cogni.libs.sparqlservice.impl.JenaModelSparqlService;
 
 public class SparqlUtilsTest {
+  private static final String URI = "https://fedlex.data.admin.ch/eli/oc/2023/423/legal-analysis";
+  private static final String GRAPH_URI = URI + "/graph";
   private final JenaModelSparqlService jenaModelSparqlService = new JenaModelSparqlService();
 
   @Test
   public void test() {
-    initializeSource();
-    confirmResult();
+    testEscape("A\ncomment"); // test newline
+    testEscape("A\\comment"); // test backslash
+    testEscape("A\bcomment"); // test backspace
+    testEscape("I'mAComment"); // test single quote
+    testEscape("A\fcomment"); // test form feed
+    testEscape("A\tcomment");  // test tab
+    testEscape("A\rcomment"); // test carriage return
+    testEscape("com\"ment"); // test double quotes
   }
 
-  private void initializeSource() {
+  private void testEscape(String value) {
+    initializeSource(value);
+    confirmResult(value);
+  }
+
+  private void initializeSource(String value) {
     jenaModelSparqlService.executeUpdateQuery(
       "PREFIX jolux: <http://data.legilux.public.lu/resource/ontology/jolux#> " +
       "INSERT DATA { " +
-      "  GRAPH <https://fedlex.data.admin.ch/eli/oc/2023/423/legal-analysis/graph> { " +
-      "    <https://fedlex.data.admin.ch/eli/oc/2023/423/legal-analysis> a jolux:LegalAnalysis. " +
-      "    <https://fedlex.data.admin.ch/eli/oc/2023/423/legal-analysis> jolux:impactFromLegalResourceComment " + "'" + SparqlUtils.escapeLiteral("A\ncomment") + "'" +
+      "  GRAPH <" + GRAPH_URI + "> { " +
+      "    <" + URI + "> a jolux:LegalAnalysis. " +
+      "    <" + URI + "> jolux:impactFromLegalResourceComment \"" + SparqlUtils.escapeString(value) + "\"" +
       "  }" +
       "}"
     );
   }
 
-  private void confirmResult() {
+  private void confirmResult(String value) {
     String query = "PREFIX jolux: <http://data.legilux.public.lu/resource/ontology/jolux#>\n" +
                    "SELECT ?comment\n" +
                    "WHERE {\n" +
-                   "  GRAPH <https://fedlex.data.admin.ch/eli/oc/2023/423/legal-analysis/graph> {\n" +
-                   "    <https://fedlex.data.admin.ch/eli/oc/2023/423/legal-analysis> a jolux:LegalAnalysis ;\n" +
-                   "                                                                  jolux:impactFromLegalResourceComment ?comment .\n" +
+                   "  GRAPH <" + GRAPH_URI + "> {\n" +
+                   "    <" + URI + "> a jolux:LegalAnalysis ;\n" +
+                   "                  jolux:impactFromLegalResourceComment ?comment .\n" +
                    "  }\n" +
                    "}";
     jenaModelSparqlService.executeSelectQuery(query, resultSet -> {
-      resultSet.forEachRemaining(result -> Assertions.assertEquals(result.get("comment")
-                                                                         .toString(), "A\ncomment"));
+      resultSet.forEachRemaining(result -> Assertions.assertEquals(value, result.get("comment")
+                                                                                .asLiteral()
+                                                                                .getValue()));
       return null;
     });
+    jenaModelSparqlService.dropGraph(GRAPH_URI);
   }
 }
