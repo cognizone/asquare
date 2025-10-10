0f27fb02783b358d322b0997d93fee2d9a160b02
natan-cox-cognizone <natan.cox@cogni.zone>
2025-10-10 15:01:16 +0200

    removed ElasticHelper helper methods which cannot be used

--- elastic-components/src/main/java/zone/cogni/asquare/service/elasticsearch/ElasticHelper.java
+++ elastic-components/src/main/java/zone/cogni/asquare/service/elasticsearch/ElasticHelper.java
@@ -3,8 +3,6 @@ package zone.cogni.asquare.service.elasticsearch;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
@@ -13,22 +11,27 @@ import java.io.IOException;
 public enum ElasticHelper {
   ;
 
-  @Nonnull
-  public static ObjectNode queryBuildToSearchRequest(@Nonnull QueryBuilder queryBuilder) {
-    return queryBuildToSearchRequest(queryBuilder, null);
-  }
-
-  @Nonnull
-  public static ObjectNode queryBuildToSearchRequest(@Nonnull QueryBuilder queryBuilder, @Nullable Integer size) {
-    SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().query(queryBuilder);
-    if (null != size) searchSourceBuilder.size(size);
-    try {
-      return (ObjectNode) new ObjectMapper().readTree(searchSourceBuilder.toString());
-    }
-    catch (IOException e) {
-      throw new RuntimeException("Failed to create ObjectNode from SearchSourceBuilder", e);
-    }
-  }
+  // NOTE: queryBuildToSearchRequest methods have been removed due to Elasticsearch licensing concerns.
+  //
+  // OLD CODE (removed):
+  //   queryBuildToSearchRequest(QueryBuilder queryBuilder)
+  //   queryBuildToSearchRequest(QueryBuilder queryBuilder, Integer size)
+  //
+  // These methods took an Elasticsearch QueryBuilder object, converted it to SearchSourceBuilder,
+  // then serialized it to JSON (ObjectNode). The optional size parameter set the result size.
+  //
+  // MIGRATION GUIDE:
+  // Build your Elasticsearch query JSON directly using Jackson ObjectNode instead of QueryBuilder.
+  //
+  // Example replacement:
+  //   OLD: ElasticHelper.queryBuildToSearchRequest(QueryBuilders.matchQuery("field", "value"), 10)
+  //   NEW: ObjectMapper mapper = new ObjectMapper();
+  //        ObjectNode query = mapper.createObjectNode();
+  //        query.putObject("query").putObject("match").put("field", "value");
+  //        query.put("size", 10);
+  //
+  // This approach avoids the Elasticsearch JAR dependency (Elastic License 2.0) and keeps the
+  // project Apache 2.0 compatible.
 
   @Nonnull
   public static ObjectNode buildFindAllQuery(@Nonnull String typeClassId) {
